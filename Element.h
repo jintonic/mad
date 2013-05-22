d65b9db21db3c2291ddc6b2d352f499c49f8cab8
Jing Liu <jing.leon@gmail.com>
2013-05-22 23:28:04 +0900

    added Print()

--- Element.h
+++ Element.h
@@ -10,37 +10,39 @@ namespace MAD { class Element; }
 
 class MAD::Element : public TGeoElement
 {
-   private:
+   protected:
       Double_t FF(Double_t *x, Double_t *parameters);
-      Double_t CNNSdXSF(Double_t *x, Double_t *parameters);
+      Double_t CNNSdXS2D(Double_t *x, Double_t *parameters);
       Double_t CNNSdXSEr(Double_t *x, Double_t *parameters);
       Double_t CNNSdXSEv(Double_t *x, Double_t *parameters);
 
-   protected:
       TF1 *fF2;    // form factor in TF1 format
-      TF2 *fDXS;   // CNNS differencial cross section in TF2 format
+      TF2 *fDXS2D; // CNNS differencial cross section in TF2 format
       TF1 *fDXSEr; // CNNS differencial cross section in TF1 format
       TF1 *fDXSEv; // CNNS differencial cross section in TF1 format
 
    public:
-      Element() : TGeoElement(), fF2(0), fDXS(0), fDXSEr(0), fDXSEv(0) {};
-      Element(Int_t nIsotopes) : TGeoElement("","",nIsotopes), 
-      fF2(0), fDXS(0), fDXSEr(0), fDXSEv(0) {};
+      Element() : TGeoElement(), fF2(0), fDXS2D(0), fDXSEr(0), fDXSEv(0) {};
+      Element(const char* name, const char* title, Int_t nIsotopes) : 
+         TGeoElement(name, title, nIsotopes), 
+         fF2(0), fDXS2D(0), fDXSEr(0), fDXSEv(0) {};
       virtual ~Element();
 
       Double_t R() const; // average nuclear radius
       Double_t M() const; // average nuclear mass
 
+      virtual void Print(Option_t *option="");
+
       Double_t F2(Double_t nuclearRecoilEnergy); // form factor squared
-      TF1* FormFactor2(Double_t maxNuclearRecoilEnergy=50*UNIC::keV);
+      TF1* FF2(Double_t maxNuclearRecoilEnergy=50*UNIC::keV);
 
       // CNNS differential cross section
       Double_t CNNSdXS(Double_t nuclearRecoilEnergy, Double_t neutrinoEnergy);
-      TF2* CNNSdXSF2(Double_t maxNuclearRecoilEnergy=50*UNIC::keV, 
+      TF2* FCNNSdXS2D(Double_t maxNuclearRecoilEnergy=50*UNIC::keV, 
             Double_t maxNeutrinoEnergy=100*UNIC::MeV);
-      TF1* CNNSdXSFEr(Double_t neutrinoEnergy,
+      TF1* FCNNSdXSEr(Double_t neutrinoEnergy,
             Double_t maxNuclearRecoilEnergy=50*UNIC::keV);
-      TF1* CNNSdXSFEv(Double_t nuclearRecoilEnergy,
+      TF1* FCNNSdXSEv(Double_t nuclearRecoilEnergy,
             Double_t maxNeutrinoEnergy=100*UNIC::MeV);
 
       ClassDef(Element,1);
