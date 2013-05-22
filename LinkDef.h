a1e4168053cfb0b3fc1c2abda7e664036dcc93fb
Jing Liu <jing.leon@gmail.com>
2013-05-22 23:31:45 +0900

    added Material

--- LinkDef.h
+++ LinkDef.h
@@ -2,6 +2,7 @@
 #pragma link C++ namespace MAD;
 #pragma link C++ class MAD::Isotope+;
 #pragma link C++ class MAD::Element+;
+#pragma link C++ class MAD::Material+;
 #pragma link C++ class MAD::NaturalXe+;
 #pragma link C++ class MAD::LiquidXenon+;
 #endif
