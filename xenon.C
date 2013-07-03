8fdde8428672986e5894538d7b415b1c6a6eaf6b
Jing LIU <jingliu@x200t.km.icrr.u-tokyo.ac.jp>
2013-07-03 22:39:16 +0900

    added f2.eps

--- xenon.C
+++ xenon.C
@@ -6,6 +6,7 @@ using namespace MAD;
 using namespace UNIC;
 
 #include <TF2.h>
+#include <TLegend.h>
 #include <TCanvas.h>
 #include <TGraphErrors.h>
 
@@ -40,4 +41,30 @@ int main()
    can->Print("xenon.ps");
 
    can->Print("xenon.ps]");
+
+   can->Clear();
+   can->SetLogx(0);
+
+   TF1 *f1fm = (TF1*) natXe->FF2()->Clone();
+   natXe->SetS(0.5*fermi);
+   TF1 *f5fm = (TF1*) natXe->FF2()->Clone();
+
+   f1fm->SetLineColor(kGray);
+   f1fm->SetLineWidth(2);
+   f5fm->SetLineColor(kRed);
+   f5fm->SetLineWidth(1);
+   f5fm->SetLineStyle(kDashed);
+
+   f1fm->Draw();
+   f5fm->Draw("same");
+
+   TLegend *leg = new TLegend(0.4,0.65,0.8,0.88);
+   leg->SetFillColor(0);
+   leg->SetBorderSize(0);
+   leg->SetHeader("nuclear skin thickness:");
+   leg->AddEntry(f1fm,"1 fm","l");
+   leg->AddEntry(f5fm,"0.5 fm","l");
+   leg->Draw();
+
+   can->Print("f2.eps");
 }
