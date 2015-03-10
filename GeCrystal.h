0072b6e57e88cc3722dd47a26db9b511b13d1a8e
Jing LIU <jing.liu@usd.edu>
2015-03-10 16:21:14 -0500

    added more mobility functions

--- GeCrystal.h
+++ GeCrystal.h
@@ -8,12 +8,18 @@ namespace MAD { class GeCrystal; }
 class MAD::GeCrystal : public Material
 {
    public:
+      Double_t T; // temperature in K
+   public:
       GeCrystal(const char *name="GeCrystal", const char *title="Ge Crystal")
          : Material(name, title) {}; 
       virtual ~GeCrystal() {};
 
-      Double_t MuE(Int_t idx=100, Double_t T=0, Double_t E=0);
-      Double_t MuH(Int_t idx=100, Double_t T=0, Double_t E=0);
+      Double_t MuEn(Int_t idx=100);
+      Double_t MuHn(Int_t idx=100);
+      Double_t MuEi(Double_t n=0);
+      Double_t MuHi(Double_t n=0);
+      Double_t MuE(Int_t idx=100, Double_t n=0);
+      Double_t MuH(Int_t idx=100, Double_t n=0);
       Double_t Epsilon();
 
       ClassDef(GeCrystal,1);
