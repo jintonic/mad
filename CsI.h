bc992e26a4da94b823c711eaea7a6511214ccade
Jing LIU <jing.liu@usd.edu>
2017-08-31 18:26:13 -0500

    added Sp

--- CsI.h
+++ CsI.h
@@ -4,17 +4,26 @@
 #include "Material.h"
 #include "Cs.h"
 #include "I.h"
-class TGraphErrors;
 
-namespace MAD{class CsI;}
-class MAD::CsI:public Material
+namespace MAD { class CsI; }
+
+class MAD::CsI: public Material
 {
-  public:
-    CsI(const char *name="CsI", const char *title="CsI")
-      :Material(name , title){AddElement(new I(),1);AddElement(new Cs(),1);};
-    virtual ~CsI();
+   public:
+      CsI(const char *name="CsI", const char *title="CsI")
+         :Material(name , title)
+         { AddElement(new I(), 1); AddElement(new Cs(), 1); }
+      virtual ~CsI() {};
 
-    classDef(CsI,1);
-}
+      TGraph* Sp(); ///< Electronic stopping power plot for proton
+
+      /**
+       * Electronic stopping power for proton.
+       * Ref. https://physics.nist.gov/PhysRefData/Star/Text/PSTAR.html
+       */
+      double Sp(double protonEnergy);
+
+      ClassDef(CsI,1);
+};
 
 #endif
